29b5f732c37a7ac3f28669b894d09aa382c5d895
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2023-04-08 13:55:51 +0300

    Update taiko.sh

--- taiko.sh
+++ taiko.sh
@@ -50,14 +50,13 @@ sudo apt-get install \
     
 apt install docker.io -y    
 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sudo apt install docker-ce -y
 
 # download binary
 cd $HOME
